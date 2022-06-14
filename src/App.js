9abf3482b166f2b5f5a98a360760cf5c5ac72458
veronicapava <veronicapava@gmail.com>
2022-06-14 14:50:13 -0500

    Actualizacion del App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import ButtonTerm from "./components/buttons/ButtonTerm";
-import CheckBox from "./components/buttons/CheckBox";
+
 import InputNumDoc from "./components/inputs/InputNumDoc";
-import InputTypeDoc from "./components/inputs/InputTypeDoc";
 
 function App() {
   return (
     <div className="container mt-5">
-      <InputTypeDoc />
       <InputNumDoc />
-      <ButtonTerm />
-      <CheckBox />
     </div>
   );
 }
